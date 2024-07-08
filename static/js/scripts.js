323dc0f5ade504140d979d9fb2ab7bbbe4de806a
ethankrol <ethanmkrol@gmail.com>
2024-07-08 10:53:34 -0500

    moved js and css scripts to respective files

---
+++ static/js/scripts.js
@@ -0,0 +1,31 @@
+const value = document.querySelector("#value");
+        const radius = document.querySelector("#radius");
+        
+        value.textContent = radius.value;
+
+        var map = L.map('map').setView([29.6520, -82.3250], 13);
+    
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    
+        var marker = L.marker([29.6520, -82.3250]).addTo(map)
+            .bindPopup('University of Florida<br>Go Gators!')
+            .openPopup();
+
+        var circle = L.circle(marker.getLatLng(), {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: radius.value * 1609.344
+            }).addTo(map);
+        
+        radius.addEventListener("input", (e) => {
+            value.textContent = e.target.value;
+            circle.setRadius(e.target.value * 1609.344);
+        });
+
+        map.on("click", function(e){
+            marker.setLatLng(e.latlng);
+            circle.setLatLng(e.latlng);
+        });
