13912e100cb6d19aa14858ef8dac681569a7241d
Soor Hansalia <soorhansalia@icloud.com>
2024-07-28 16:53:18 -0400

    u

--- static/js/visualization.js
+++ static/js/visualization.js
@@ -2,3 +2,7 @@ let sort_btn = document.getElementById("sort_btn");
 let bars_container = document.getElementById("bars_container");
 let speed = document.getElementById("speed");
 let unsorted_nums = [];
+
+for (let x = 0; x < 209867; x++) {
+    unsorted_nums.push(Math.random());
+}
