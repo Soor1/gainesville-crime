98a62390dcfe9b6bb7d294bbf8474a699b20a10d
ethankrol <ethanmkrol@gmail.com>
2024-08-02 21:59:47 -0500

    Adding app.py database backend

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request, session, g
+import sqlite3
+from math import radians, cos, sin, sqrt, atan2
+from datetime import datetime
 import json
 
 app = Flask(__name__)
+
+def get_db():
+    if 'db' not in g:
+        db = sqlite3.connect('db/crime.db')
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+def haversine(lat1, lon1, lat2, lon2):
+    R = 3959  
+
+    dlat = radians(lat2 - lat1)
+    dlon = radians(lon2 - lon1)
+    a = sin(dlat / 2) ** 2 + cos(radians(lat1)) * cos(radians(lat2)) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+    return distance
 
 @app.route('/snap')
 def getSnap():
@@ -20,6 +41,49 @@ def about():
 @app.route('/visualization')
 def visualization():
     return render_template('visualization.html')
+
+@app.route('/api/crimes', methods=['GET'])
+def get_crimes():
+    start_time = request.args.get('start_time')
+    end_time = request.args.get('end_time')
+    latitude = float(request.args.get('latitude'))
+    longitude = float(request.args.get('longitude'))
+    radius = float(request.args.get('radius'))
+
+    conn = get_db()
+    cursor = conn.cursor()
+    
+    start_time = datetime.strptime(start_time, '%H:%M').time()
+    end_time = datetime.strptime(end_time, '%H:%M').time()
+    
+    cursor.execute("SELECT * FROM incidents")
+    rows = cursor.fetchall()
+    conn.close()
+    
+    filtered_crimes = []
+    for row in rows:
+        crime_time = datetime.strptime(row['Offense_Date'], '%Y-%m-%d %H:%M:%S').time()
+        crime_lat = row['Latitude']
+        crime_lon = row['Longitude']
+        
+        if start_time <= crime_time <= end_time:
+            distance = haversine(latitude, longitude, crime_lat, crime_lon)
+            if distance <= radius:
+                filtered_crimes.append({
+                    "ID": row['ID'],
+                    "Incident_Type": row['Incident_Type'],
+                    "Offense_Date": row['Offense_Date'],
+                    "Latitude": row['Latitude'],
+                    "Longitude": row['Longitude']
+                })
+    return jsonify(filtered_crimes)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
